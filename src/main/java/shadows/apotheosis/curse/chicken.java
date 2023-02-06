50a285b3243e9203576487ee78e1315078fff9ef
Shadows-Of-Ice <124480915+Shadows-Of-Ice@users.noreply.github.com>
2023-02-06 00:13:02 -0500

    bugfix

---
+++ src/main/java/shadows/apotheosis/curse/chicken.java
@@ -0,0 +1,32 @@
+package com.shadows.apotheosis.curse;
+
+import net.minecraft.server.MinecraftServer;
+
+public class chicken {
+
+
+    long x = event.world.getWorldTime();
+    int[] c = new int[3];
+    int y = rand(minecraft.server.player.list);
+
+    static void spawnChicken() {
+
+        world.level.entity.chicken -> new world.level.gameevent.curse;
+        for(int a = 30; a < 0; a++) {
+            entity.chicken = world.level.gameevent.curse;
+            entity.chicken.x = c[0];
+            entity.chicken.y = c[1];
+            entity.chicken.z = c[2];
+        }
+    }
+
+    if (x % 100 == 0) {
+        for(int z = 2; z < 0; z++) {
+            c[0] = y.playerSpawnPointX;
+            c[1] = y.playerSpawnPointY;
+            c[2] = y.playerSpawnPointZ;
+        }
+
+        spawnChicken();
+    }
+}
